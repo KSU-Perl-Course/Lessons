504fd4b57bb5d70682ea3b62f84baa490e0543f4
kstatebioinfo <kstate.bioinformatics@gmail.com>
2013-08-27 14:12:30 -0500

    Update Fastq_to_Fasta.pl

--- Fastq_to_Fasta.pl
+++ Fastq_to_Fasta.pl
@@ -2,21 +2,21 @@
 
 use strict;
 use warnings;
-# usage: Fasta_to_Fastq.pl [fasta_file] [fastq_file]
+# usage: Fastq_to_Fasta.pl [fasta_file] [fastq_file]
 #initialized value of $i and declare variables
 my $infile=$ARGV[0];
 my $outfile=$ARGV[1];
 my $i=0;
 my($identifier, $description);
 
-#Open the file for over-writing, use the filehandle OLDFASTA
+#Open the file for over-writing, use the filehandle NEWFASTA
 open (NEWFASTA, ">$outfile");
 
-#Open the file for reading only, use the filehandle NEWFASTA
-open(OLDFASTA, "<$infile");
+#Open the file for reading only, use the filehandle OLDFASTQ
+open(OLDFASTQ, "<$infile");
 
 #Process the in file via its file handle IN
-while(<OLDFASTA>)
+while(<OLDFASTQ>)
 
 {
     if($i==0) #loop for headers
